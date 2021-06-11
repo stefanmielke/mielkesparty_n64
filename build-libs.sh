938dcb024e431921810c768df815d3a259b76fdc
Stefan Mielke <stefanmielke@fastmail.com>
2021-06-10 21:20:04 -0300

    Removing docker build from build-libs.sh

--- build-libs.sh
+++ build-libs.sh
@@ -1,3 +1,2 @@
-docker build -t libdragon-build-local .
 docker run -it --rm -v$(pwd):/libdragon/homebrew libdragon-build-local make clean -C /libdragon/homebrew/libs/libdragon
 docker run -it --rm -v$(pwd):/libdragon/homebrew libdragon-build-local make all -C /libdragon/homebrew/libs/libdragon -j
